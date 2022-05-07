fb306f236130d6c3292968e84374595acbfe364b
박재호 <inno_pjh@innogrid.com>
2022-05-07 17:42:14 +0900

    todolist;

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+import {Counter} from "./study/Counter";
+import {TodoList} from "./study/todoList";
+import {Profile } from "./study/Profile";
 
-const App: React.FC = () => {
+const App = () => {
   return (
-      <div className="App">
-        Hello Typescript
+      <div className='App'>
+        <Profile name='myName' job='job' />
+          <Counter />
+          <TodoList />
       </div>
   );
-}
-
+};
 export default App;
