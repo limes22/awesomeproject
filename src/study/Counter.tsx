3f81bc74ec6a9fda90c6c011a44dd19fdaa0023b
박재호 <inno_pjh@innogrid.com>
2022-05-10 20:37:49 +0900

    Update Profile & Counter

--- src/study/Counter.tsx
+++ src/study/Counter.tsx
@@ -2,42 +2,45 @@ import * as React from 'react';
 import {useState, useEffect, useRef} from 'react';
 
 // state type 선언
-interface Istate {
-    counter: number
-}
+// interface Istate {
+//     counter: number
+// }
 
 export function Counter() {
     // useState로 관리해 줄 state 의 type 설정
-    const [state, setState] = useState<Istate>({
-        counter: 0,
-    })
+    // const [state, setState] = useState<Istate>({
+    //     counter: 0,
+    // })
+    const [state, setState] = useState(0)
 
     const onIncrement = () => {
-        setState({
-            counter: state.counter+1
-        })
+        // setState({
+        //     counter: state.counter+1
+        // })
+        setState(state + 1)
     }
 
     const onDecrement = () => {
-        setState({
-            counter: state.counter-1
-        })
+        // setState({
+        //     counter: state.counter-1
+        // })
+        setState(state - 1)
     }
 
-    useEffect(() => {
-        console.log("now im mount");
-        return () => console.log("now im unmount");
-    }, []);
-    useEffect(() => {
-        console.log("now im update");
-    });
+    // useEffect(() => {
+    //     console.log("now im mount");
+    //     return () => console.log("now im unmount");
+    // }, []);
+    // useEffect(() => {
+    //     console.log("now im update");
+    // });
 
     return(
         <>
             <div>
                 <h2>카운터</h2>
                 <div>
-                    {state.counter}
+                    {state}
                 </div>
                 <div>
                     <button onClick={onIncrement}>+</button>
--- src/study/Profile.tsx
+++ src/study/Profile.tsx
@@ -6,23 +6,32 @@ import internal from 'stream';
 // props 받아올 값의 type 을 선언
 interface Iprops {
     name: string,
+    gender: string,
+    email: string,
+    phone: string,
+    id?: string,
 }
 
 // 함수가 활용하는 props(interface)는 다음과 같이 확장할 수 있습니다.
 
 // 인터페이스 Iprops => name: string만 가지고 있음
 // 인터페이스 Iprops2 => gender, email, phone, id를 가지고있고, 추가적으로 Iprops의 모든 선언값을 가져와서 확장 선언함
-interface Iprops2 extends Iprops{
-    gender: string,
-    email: string,
-    phone: string,
-    // id?: string
-}
+// interface Iprops2 extends Iprops{
+//     gender: string,
+//     email: string,
+//     phone: string,
+//     // id?: string // number, string, undefined, [], {}
+// }
 
-export const Profile = (props: Iprops2) => {
+export const Profile = (props: Iprops) => {
     // const { name, gender, email, phone, id } = props;
     // 비구조 할당 문법 => 오브젝트가 있다면, 해당 Key값들을 다음과 같이 추출할수 있습니다. reference: https://learnjs.vlpt.us/useful/06-destructuring.html
     const { name, gender, email, phone } = props;
+    // const name = props.name;
+    // const gender = props.gender;
+    // const email = props.email;
+    // const phone = props.phone;
+    // const value = undefined;
     return(
         <>
             <div>
