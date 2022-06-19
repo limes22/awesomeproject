71bda8ecdfae8464e5bf0db2602d74a4a7adf2e7
limes22 <howdi2002@naver.com>
2022-06-19 14:18:01 +0000

    counter

---
+++ src/Counter.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import {useState, useEffect, useRef} from 'react';
+
+
+const Counter = () => {
+    const [state, setState] = useState(0)
+    const onIncrement = () => {
+        setState(state + 1)
+    }
+    const onDecrement = () => {
+        setState(state -1)
+    }
+
+    return (
+        <>
+            <div className='Counter'>
+                <h2> 카운터 </h2>
+		<div>
+			{state}
+		</div>
+                <div>
+                    <button onClick={onIncrement}>+</button>
+                    <button onClick={onDecrement}>-</button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Counter;
