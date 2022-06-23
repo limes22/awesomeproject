56f2dabc8669e2ed051f35b4f1f7545868ac42ce
limes22 <howdi2002@naver.com>
2022-06-23 14:08:31 +0000

    ;

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -10,15 +10,15 @@ export const Counter = (props: Iprops) => {
     const {state :newState} = props;
     console.log('나는카운터:', newState);
     useEffect(() => {
-        
+
     }, []);
     const [state, setState] = useState(newState);
     // const {myNumber: temp} = props; 구조분해를 사용하는 이유 props에 정의 되어 있지 않은 값이 들어왔을때 예외처리가 손쉽다.
     const onIncrement = () => {
-        setState(state + 1)
+        setState(state + 1);
     };
     const onDecrement = () => {
-        setState(state - 1)
+        setState(state - 1);
     };
 
     return (
@@ -34,7 +34,7 @@ export const Counter = (props: Iprops) => {
                 </div>
             </div>
         </>
-    )
-}
+    );
+};
 
 export default Counter;
